7d474b59d80519840960f3cc5225d2751400c72b
Jimmy Vo <jimmyvo@Jimmys-MacBook-Pro.local>
2024-08-15 01:23:40 -0400

    add comments to tests

--- cypress/e2e/home-page.cy.ts
+++ cypress/e2e/home-page.cy.ts
@@ -1,8 +1,8 @@
 describe('check a11y on home page', () => {
   it('should run a11y test', () => {
-    cy.visit('/')
-    cy.injectAxe()
-    cy.wait(300)
+    cy.visit('/')   // Load the page in the Cypress browser
+    cy.injectAxe()      // Attach the Axe engine to the DOM
+    cy.wait(300)    // Perform accessibility checks on the DOM
 
     cy.url().should('include', '/resource');
     cy.checkA11y();
--- cypress/e2e/issues-page.cy.ts
+++ cypress/e2e/issues-page.cy.ts
@@ -6,18 +6,22 @@ describe('check a11y on issues page', () => {
   })
 
   it('should click `with-issues` page button and run a11y test', () => {
+    // go to issues page
+    cy.get('button:contains("Page with Issues")').click();
+    // wait for page to render
+    cy.wait(300);
 
-    cy.visit('/')
-    cy.injectAxe()
-    cy.wait(300)
+    cy.url().should('include', '/with-issues');
+    cy.checkA11y();
+  })
 
+  it('should click `with-issues` page button, open accordion and run a11y test', () => {
     cy.get('button:contains("Page with Issues")').click();
     cy.wait(300);
 
     cy.get('mat-expansion-panel-header').click();
     cy.wait(300);
 
-    cy.url().should('include', '/with-issues');
     cy.checkA11y();
   })
 
